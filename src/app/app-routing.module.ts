f769dd13d44bc089ec92bedb4525cf73eafaefcd
Kousik Roy <kaushikroy1001@gmail.com>
2022-03-29 16:14:54 +0530

    Router added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NotfoundComponent } from './notfound/notfound.component';
-const routes: Routes = [{ path: 'signin', component: NotfoundComponent }];
+import { FormComponent } from './form/form.component';
+const routes: Routes = [
+  { path: '', component: FormComponent },
+  { path: '**', component: NotfoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
-export const RoutingComponents = [NotfoundComponent];
+export const RoutingComponents = [NotfoundComponent, FormComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule, RoutingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
-  declarations: [AppComponent, RoutingComponents],
+  declarations: [AppComponent, RoutingComponents, FormComponent],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
